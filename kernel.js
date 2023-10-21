bf2ae407e10cd1429c8c733791c0bb4c86a71f03
A Language <52973315+alng-org@users.noreply.github.com>
2023-10-21 17:06:54 +0800

    Add files via upload

--- kernel.js
+++ kernel.js
@@ -1,147 +1,93 @@
 class map{
-    constructor(left,right,has_quote){
-        this.left=left;
-        this.right=right;
-        this.has_quote=has_quote;
-    }
-    tree(src){
-        
-    }
-}
-function struct(src){
-    //(a#b) => [a,#, b]
-    //(a#b#c..) => [a,#,b,#,c,#,...]
-    //Suppose str is paired
-    //Need to be redesign
-    let data = []; // now building
-    let pre_atom=``; //the previous atom
-    let push_if=(data,pre_atom)=>{ //push the empty atom
-        switch(pre_atom){
-            case `(`:
-            case `→`:
-                data.push(``);
-                break;
-            default:
-                break;
-        }
-        return data;
-    }
-    let st=[]; // building pause for wait the sub-expr building
-    for(let atom of src.match(/\(|→|\)|[^\(→\)]+/g)){
-        switch(atom){
-            case `(`:
-                st.push(data);
-                data=[];
-                break;
-            case `→`:
-                push_if(data,pre_atom);
-                data.push(atom);
-                break;
-            case `)`:
-                push_if(data,pre_atom);
-                let tp=data;
-                data=st.pop();
-                data.push(tp);
-                break;
-            default:
-                data.push(atom);
-        }
-        pre_atom=atom;
-    }
-    return data;
-}
-function* gen_atom(src){
-    let text_req=true;
-    let gen_base= function*(atom,err=null){
-        if(text_req == true){
-            yield atom;
-        }else if(err !== null){
-            yield err;
-        }else{
-            //PASS
-        }
-    };
-    for(let atom of src.match(/\(|→|\)|[^\(→\)]+/g)){
-        switch(atom){ //fallthough is right
-            case `→`:
-            case `)`:
-                yield* gen_base(``);
+    #map=[];
+    static *#gen_atom(src){
+        let text_req=true;
+        let gen_base= function*(atom,err=null){
+            if(text_req == true){
                 yield atom;
-                break;
-            case `(`:
-            default:
-                yield* gen_base(atom,[atom]);
-        }
-        switch(atom){ //fallthough is right
-            case `→`:
-            case `(`:
-                text_req=true;
-                break;
-            case `)`:
-            default:
-                text_req=false;
-        }
-    }
- }
- function f(src){
-    for(let atom of gen_atom(src)){
-        console.log(atom);
-    }
- }
- function tomap(src){
-    let map=[];
-    let st=[];
-    /*let expand=(xs)=>{
-        let t=xs[xs.length-1];
-        if(typeof(t) === typeof([])){
-            return xs.concat(xs.pop());
-        }else{
-            return xs;
-        }
-    }*/
-    let last=(xs)=>xs[xs.length-1];
-    let format=(xs)=>{
-        xs=xs.reduce((left,right)=>{
-            if(last(left) === null){
-                left.pop();
-                if(typeof(last(left)) !== typeof([]) || last(left)[0] !== null){
-                    last(left) =[null,last(left)]; //error == ReferenceError: Invalid left-hand side in assignment
-                }else{
-                    //PASS
-                }
-                last(left)=last(left).concat(right);
+            }else if(err !== null){
+                yield err;
             }else{
-                left=left.concat(right);
+                //PASS
             }
-            return left;
-        },[]);
-        if(typeof(last(xs)) === typeof([]) && last(xs)[0] !== null){
-            xs=xs.concat(xs.pop());
-        }else{
-            //PASS
-        }
-        return xs;
-    };
-    for(let atom of gen_atom(src)){
-        if(typeof(atom) == typeof([])){
-            atom=atom[0];
-            map.push(null);
-        }
-        switch(atom){
-            case '(':
-                st.push(map);
-                map=[];
-                break;
-            case ')':
-                let par=st.pop();
-                par.push(format(map));
-                //par.push(map);
-                map=par;
-                break;
-            default:
-                map.push(atom);
+        };
+        for(let atom of src.match(/\(|→|\)|[^\(→\)]+/g)){
+            switch(atom){ //fallthough is right
+                case `→`:
+                case `)`:
+                    yield* gen_base(``);
+                    yield atom;
+                    break;
+                case `(`:
+                default:
+                    yield* gen_base(atom,[atom]);
+            }
+            switch(atom){ //fallthough is right
+                case `→`:
+                case `(`:
+                    text_req=true;
+                    break;
+                case `)`:
+                default:
+                    text_req=false;
+            }
         }
     }
-    return format(map);
-    //return map;
- }
+    *[Symbol.iterator](){
+        for(let i=0;i<this.#map.length-1;i=i+1){
+            yield this.#map[i];
+        }
+        let v=this.#map[this.#map.length-1];
+        if(typeof(v) === typeof(new map(null))){
+            yield* v[Symbol.iterator]();
+        }else{
+            yield v;
+        }
+    }
+    toString(){
+        let s="";
+        for(let v of this.#map){
+            if(typeof(v) === typeof("")){
+                s=s.concat(v);
+            }else{
+                s=s.concat(`(${v.toString()})`);
+            }
+        }
+        return s;
+    }
+    valueOf(){
+        return this.toString();
+    }
+    static #group(list){
+        let v=new map(null);
+        v.#map=list;
+        return v;
+    }
+    constructor(src){
+        let st=[];
+        if(typeof(src) === typeof("")){
+            for(let atom of map.#gen_atom(src)){
+                if(typeof(atom) == typeof([])){
+                    atom=atom[0];
+                    this.#map.push(null);
+                }
+                switch(atom){
+                    case '(':
+                        st.push(this.#map);
+                        this.#map=[];
+                        break;
+                    case ')':
+                        let par=st.pop();
+                        par.push(map.#group(this.#map));
+                        this.#map=par;
+                        break;
+                    default:
+                        this.#map.push(atom);
+                }
+            }
+            return this;
+        }else{
+            return this;
+        }
+    }
+}
