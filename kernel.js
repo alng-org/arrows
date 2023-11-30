99246f05c50ecbe876f84e426788bb84d3be378e
A Language <52973315+alng-org@users.noreply.github.com>
2023-11-30 15:51:39 +0800

    Add files via upload

--- kernel.js
+++ kernel.js
@@ -35,11 +35,11 @@ class map{
         }
     }
     static #unwrap(val){
-        if(val instanceof map){
-            if(val.#map.length === 1 && val.#map[0] instanceof map){
-                return map.#unwrap(val.#map[0]);
-            }else if(val.#map.length === 1 && typeof(val.#map[0]) === typeof(``)){
-                return val.#map[0];
+        if(type(val) === type(new map(null))){
+            if(val.#length() === 1 && type(val.#at(0)) === type(new map(null))){
+                return map.#unwrap(val.#at(0));
+            }else if(val.#length() === 1 && type(val.#at(0)) === type(``)){
+                return val.#at(0);
             }else{
                 return val;
             }
@@ -48,11 +48,11 @@ class map{
         }
     }
     *[Symbol.iterator](){
-        for(let i=0;i<this.#map.length-1;i=i+1){
-            yield map.#unwrap(this.#map[i]);
+        for(let i=0;i<this.#length()-1;i=i+1){
+            yield map.#unwrap(this.#at(i));
         }
-        let v=this.#map[this.#map.length-1];
-        if(v instanceof map){
+        let v=this.#at(-1);
+        if(type(v) === type(new map(null))){
             yield* v[Symbol.iterator]();
         }else{
             yield v;
@@ -61,7 +61,7 @@ class map{
     uni_quote(){ //a deep copy of this with remove addtional quote
         let v=new map(null);
         for(let i of this){
-            if(typeof(i) === typeof(``)){
+            if(type(i) === type(``)){
                 v.#push(i);
             }else{
                 v.#push(i.uni_quote());
@@ -72,10 +72,12 @@ class map{
     toString(){
         let s="";
         for(let v of this.#map){
-            if(typeof(v) === typeof("")){
+            if(type(v) === type(``)){
                 s=s.concat(v);
-            }else if(v instanceof map){
+            }else if(type(v) === type(new map(null))){
                 s=s.concat(v.toString());
+            }else if(type(v) === type([]) && v.length === 2){
+                s=s.concat(v[0]).concat(v[1].toString());
             }else{
                 //PASS
             }
@@ -96,10 +98,30 @@ class map{
         this.#map=[];
         return this;
     }
-    #last(){
-        return this.#map[this.#map.length-1];
+    #length(){
+        return this.#map.length;
     }
-    static #fgroup(gen){ //TODO: need to redesign
+    #at(index){
+        let len=this.#length();
+        if(0 <= index && index < len){
+            return this.#map[index];
+        }else if(-len <= index && index <0){
+            return this.#map[len+index];
+        }else{
+            return null;
+        }
+    }
+    #last(){
+        return this.#at(-1);
+    }
+    static #farrow(arrow){
+        if(type(arrow) === type(``)){
+            return `→`;
+        }else{
+            return `[→]`;
+        }
+    }
+    static #fgroup(gen){
         let x=gen.next();
         let v=new map(null);
         while(x.done === false){
@@ -107,6 +129,14 @@ class map{
                 case '(':
                     v.#push(map.#fgroup(gen));
                     gen=v.#last().#pop(); // it's not need to do so...
+                    if(type(v.#at(-3)) === type(``)
+                    && map.#farrow(v.#at(-2)) === `[→]`){
+                        let val=v.#pop();
+                        v.#pop();
+                        v.#push([v.#pop(),val]);
+                    }else{
+                        //PASS
+                    }
                     break;
                 case ')':
                     v.#push(gen);
@@ -120,8 +150,8 @@ class map{
         v.#push(gen);
         return v;
     }
-    constructor(src){
-        if(typeof(src) === typeof(``) && src[0] === `(`){
+    constructor(src=null){
+        if(type(src) === type(``) && src[0] === `(`){
             let gen=map.#gen_atom(src);
             gen.next();
             let v=map.#fgroup(gen);
@@ -138,7 +168,7 @@ class map{
     #apply(dict){
         let v=new map(null);
         for(let x of this){
-            if(typeof(x) === typeof(``)){
+            if(type(x) === type(``)){
                 if(Object.hasOwn(dict,x) === true){
                     v.#push(dict[x]);
                 }else{
@@ -149,5 +179,19 @@ class map{
             }
         }
         return v;
+    }
+}
+function type(val){
+    //reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+    let btype=typeof(val);
+    let ctype=val?.constructor?.name;
+    if(val === null){
+        return `null`;
+    }else if(btype !== `object`){
+        return btype;
+    }else if(typeof(ctype) === typeof(``) && ctype !== ``){
+        return ctype;
+    }else{
+        return `?`;
     }
 }
