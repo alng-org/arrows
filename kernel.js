cbea4bc0637841139571d27ffa54625ce6d7a338
A Language <52973315+alng-org@users.noreply.github.com>
2023-12-05 06:07:54 +0800

    Add files via upload

--- kernel.js
+++ kernel.js
@@ -49,14 +49,16 @@ class map{
             return `e`;
         }
     }
-    static #unwrap(val){
+    static #unwrap(val,unwarp_empty_v=false){
         if(map.#type(val,false) === `?`){
             let inner=val.#at(0);
             switch(map.#type(inner,false)){
                 case `?`:
-                    return map.#unwrap(inner);
+                    return map.#unwrap(inner,unwarp_empty_v);
                 case `v`:
-                    if(inner[0] !== ``){
+                    if(unwarp_empty_v === true){
+                        return inner;
+                    }else if(inner[0] !== ``){
                         return inner;
                     }else{
                         //fall though
@@ -68,12 +70,117 @@ class map{
             return val;
         }
     }
-    static unwarp(v){
-        return map.#unwrap(v);
+    static #bindable(v){
+        let x=map.#unwrap(v,true);
+        if(map.#type(v) === `v` && x.length === 1){
+            return  x[0];
+        }else{
+            return null;
+        }
+    }
+    static #bind(v,val){
+        if(map.#bindable(v) !== null){
+            return  [v,val];
+        }else{
+            return v;
+        }
+    }
+    #apply(dict){
+        let each=(v)=>{
+            let key=map.#bindable(v);
+            if(key !== null && Object.hasOwn(dict,key) === true){
+                return map.#bind(v,dict[key]);
+            }else if([`→`,`v`].includes(map.#type(v)) === true){
+                return v;
+            }else{
+                return v.#apply(dict);
+            }
+        }
+        let v=new map();
+        v.#map=this.#map.map(each);
+        return v;
+    }
+    static #farrow(arrow){
+        switch(arrow){
+            case `→`:
+                return Symbol.for(`→`);
+            case `[→]`:
+                return Symbol.for(`[→]`);
+            case Symbol.for(`→`):
+                return `→`;
+            case Symbol.for(`[→]`):
+                return `[→]`;
+            default:
+                return arrow;
+        }
+    }
+    static #fgroup(gen){
+        let x=gen.next();
+        let v=new map();
+        while(x.done === false){
+            switch(x.value){
+                case '(':
+                    v.#push(map.#fgroup(gen));
+                    gen=v.#last().#pop(); // it's not need to do so...
+                    if(map.#bindable(v.#at(-3)) !== null 
+                    && map.#farrow(v.#at(-2)) === `[→]`){
+                        let val=v.#pop();
+                        v.#pop();
+                       v.#push(map.#bind(v.#pop(),val));
+                    }else{
+                        //PASS
+                    }
+                    break;
+                case ')':
+                    v.#push(gen);
+                    return v;
+                default:
+                    v.#push(x.value);
+            }
+            x=gen.next();
+        }
+        v.#empty();
+        v.#push(gen);
+        return v;
+    }
+    #pop(){
+        return this.#map.pop();
+    }
+    #push(val){
+        this.#map.push(val);
+        return this;
+    }
+    #empty(){
+        this.#map=[];
+        return this;
+    }
+    #length(){
+        return this.#map.length;
+    }
+    #at(index){
+        let len=this.#length();
+        if(0 <= index && index < len){
+            return this.#map[index];
+        }else if(-len <= index && index <0){
+            return this.#map[len+index];
+        }else{
+            return null;
+        }
+    }
+    #last(){
+        return this.#at(-1);
+    }
+    //test code
+    static unwarp(v,uev = false){
+        return map.#unwrap(v,uev);
     }
     static type(v){
         return map.#type(v);
     }
+    apply(dict){
+        return this.#apply(dict);
+    }
+    //test
     *[Symbol.iterator](){
         for(let i=0;i<this.#length()-1;i=i+1){
             yield map.#unwrap(this.#at(i));
@@ -117,79 +224,6 @@ class map{
     valueOf(){
         return this.toString();
     }
-    #pop(){
-        return this.#map.pop();
-    }
-    #push(val){
-        this.#map.push(val);
-        return this;
-    }
-    #empty(){
-        this.#map=[];
-        return this;
-    }
-    #length(){
-        return this.#map.length;
-    }
-    #at(index){
-        let len=this.#length();
-        if(0 <= index && index < len){
-            return this.#map[index];
-        }else if(-len <= index && index <0){
-            return this.#map[len+index];
-        }else{
-            return null;
-        }
-    }
-    #last(){
-        return this.#at(-1);
-    }
-    static #farrow(arrow){
-        switch(arrow){
-            case `→`:
-                return Symbol.for(`→`);
-            case `[→]`:
-                return Symbol.for(`[→]`);
-            case Symbol.for(`→`):
-                return `→`;
-            case Symbol.for(`[→]`):
-                return `[→]`;
-            default:
-                return arrow;
-        }
-    }
-    static #fgroup(gen){
-        let x=gen.next();
-        let v=new map();
-        while(x.done === false){
-            switch(x.value){
-                case '(':
-                    v.#push(map.#fgroup(gen));
-                    gen=v.#last().#pop(); // it's not need to do so...
-                    if(map.#type(v.#at(-3)) === `v`
-                    && ((x)=>(x?.length === 1)
-                          || (type(x) === type(new map()) && x.#at(0)?.length === 1)
-                       )(map.#unwrap(v.#at(-3)))
-                    && map.#farrow(v.#at(-2)) === `[→]`){
-                        let val=v.#pop();
-                        v.#pop();
-                       v.#push([v.#pop(),val]);
-                    }else{
-                        //PASS
-                    }
-                    break;
-                case ')':
-                    v.#push(gen);
-                    return v;
-                default:
-                    v.#push(x.value);
-            }
-            x=gen.next();
-        }
-        v.#empty();
-        v.#push(gen);
-        return v;
-    }
     constructor(src=null){
         if(type(src) === type(``) && src[0] === `(`){
             let gen=map.#gen_atom(src);
@@ -205,21 +239,7 @@ class map{
             this.#map=[];
         }
     }
-    #apply(dict){
-        let v=new map();
-        for(let x of this){
-            if(type(x) === type(``)){
-                if(Object.hasOwn(dict,x) === true){
-                    v.#push(dict[x]);
-                }else{
-                    v.#push(x);
-                }
-            }else{
-                v.#push(x.#apply(dict));
-            }
-        }
-        return v;
-    }
+    
 }
 function type(val){
     //reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
